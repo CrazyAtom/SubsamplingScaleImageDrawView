834bb4ae8a684e43f0d1c1c4b9a03ba59fca55ff
HANGIL-KHB\crazy <crazyatom84@gmail.com>
2017-08-04 18:00:58 +0900

    구름 형상 미리 계산 하도록 변경

--- subsamplingscaleimagedrawview/src/main/java/com/github/crazyatom/subsamplingscaleimagedrawview/drawviews/DrawViewCloud.java
+++ subsamplingscaleimagedrawview/src/main/java/com/github/crazyatom/subsamplingscaleimagedrawview/drawviews/DrawViewCloud.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.support.annotation.NonNull;
 
 import com.github.crazyatom.subsamplingscaleimagedrawview.R;
 import com.github.crazyatom.subsamplingscaleimagedrawview.views.ImageDrawView;
+
+import java.util.ArrayList;
 
 /**
  * Created by hangilit on 2017. 7. 14..
@@ -18,7 +21,7 @@ import com.github.crazyatom.subsamplingscaleimagedrawview.views.ImageDrawView;
 public class DrawViewCloud extends BaseDrawView {
 
     final private float INTERVAL_RATIO = 0.2f;
-    private float sInterval = 0;
+    private ArrayList<ArcPiece> arcPieces;
 
     public DrawViewCloud(@NonNull ImageDrawView imageDrawView) {
         super(DrawViewType.CLOUD, imageDrawView);
@@ -37,99 +40,65 @@ public class DrawViewCloud extends BaseDrawView {
     @Override
     protected void preCalc() {
         final float min = Math.min(sRegion.width(), sRegion.height());
-        this.sInterval = min * INTERVAL_RATIO;
+        final float interval = min * INTERVAL_RATIO;
+        if (interval > 0) {
+            final float offset = interval / 2;
+            final RectF baseRect = new RectF(sRegion.left + offset, sRegion.top + offset, sRegion.right - offset, sRegion.bottom - offset);
+
+            arcPieces = new ArrayList<>();
+            // left to right
+            PointF posTop = new PointF(baseRect.left, baseRect.top);
+            PointF posBottom = new PointF(baseRect.left, baseRect.bottom);
+            int cnt = Math.round(baseRect.width() / interval);
+            for(int i = 0; i < cnt; ++i) {
+                if (i == (cnt - 1)) {
+                    arcPieces.add(new ArcPiece(new RectF(posTop.x, (posTop.y - offset), baseRect.right, (posTop.y + offset)), 180, 180));
+                    arcPieces.add(new ArcPiece(new RectF(posBottom.x, (posBottom.y - offset), baseRect.right, (posBottom.y + offset)), 180, -180));
+                } else {
+                    arcPieces.add(new ArcPiece(new RectF(posTop.x, (posTop.y - offset), (posTop.x + interval), (posTop.y + offset)), 180, 180));
+                    arcPieces.add(new ArcPiece(new RectF(posBottom.x, (posBottom.y - offset), (posBottom.x + interval), (posBottom.y + offset)), 180, -180));
+                }
+                posTop.offset(interval, 0);
+                posBottom.offset(interval, 0);
+            }
+            // top to bottom
+            PointF posLeft = new PointF(baseRect.left, baseRect.top);
+            PointF posRight = new PointF(baseRect.right, baseRect.top);
+            cnt = Math.round(baseRect.height() / interval);
+            for(int i = 0; i < cnt; ++i) {
+                if (i == (cnt - 1)) {
+                    arcPieces.add(new ArcPiece(new RectF((posLeft.x - offset), posLeft.y, (posLeft.x + offset), baseRect.bottom), 270, -180));
+                    arcPieces.add(new ArcPiece(new RectF((posRight.x - offset), posRight.y, (posRight.x + offset), baseRect.bottom), 270, 180));
+                } else {
+                    arcPieces.add(new ArcPiece(new RectF((posLeft.x - offset), posLeft.y, (posLeft.x + offset), (posLeft.y + interval)), 270, -180));
+                    arcPieces.add(new ArcPiece(new RectF((posRight.x - offset), posRight.y, (posRight.x + offset), (posRight.y + interval)), 270, 180));
+                }
+                posLeft.offset(0, interval);
+                posRight.offset(0, interval);
+            }
+        }
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        if (sRegion == null || this.sInterval == 0) {
-            return;
-        }
-
-        RectF vRect = new RectF();
-        imageDrawView.sourceToViewRect(sRegion, vRect);
-        if (vRect.width() == 0 || vRect.height() == 0) {
-            return;
-        }
-
-        final int vInterval = (int) (sInterval * imageDrawView.getScale());
-        if (vInterval ==  0) {
+        if (arcPieces == null) {
             return;
         }
 
         loadPaint();
 
-        final float offset = vInterval / 2;
-        vRect.left += offset;
-        vRect.top += offset;
-        vRect.right -= offset;
-        vRect.bottom -= offset;
-        drawLeftToRight(canvas, vRect, vInterval);
-        drawTopToBottom(canvas, vRect, vInterval);
+        try {
+            for (ArcPiece arcPiece : arcPieces) {
+                if (arcPiece.oval != null) {
+                    canvas.drawArc(imageDrawView.sourceToViewRect(arcPiece.oval, new RectF()), arcPiece.startAngle, arcPiece.sweepAngle, false, paint);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         super.onDraw(canvas);
     }
-
-    /**
-     * 좌에서 우로 타원조각 그리기
-     * @param canvas
-     * @param vRect 구름 형상이 그려질 기준 사각형 뷰 좌표
-     * @param vInterval 뷰 좌표에서의 타원 간격
-     */
-    private void drawLeftToRight(Canvas canvas, final RectF vRect, final float vInterval) {
-        final float offset = vInterval / 2.0f;
-
-        PointF posTop = new PointF(vRect.left, vRect.top);
-        PointF posBottom = new PointF(vRect.left, vRect.bottom);
-        RectF ovalTop = new RectF();
-        RectF ovalBottom = new RectF();
-
-        final int cnt = Math.round(vRect.width() / vInterval);
-        for (int i = 0; i < cnt; ++i) {
-            if (i == cnt - 1) {
-                ovalTop.set(posTop.x, (posTop.y - offset), vRect.right, (posTop.y + offset));
-                ovalBottom.set(posBottom.x, (posBottom.y - offset), vRect.right, (posBottom.y + offset));
-            } else {
-                ovalTop.set(posTop.x, (posTop.y - offset), (posTop.x + vInterval), (posTop.y + offset));
-                ovalBottom.set(posBottom.x, (posBottom.y - offset), (posBottom.x + vInterval), (posBottom.y + offset));
-            }
-            canvas.drawArc(ovalTop, 180, 180, false, paint);
-            posTop.offset(vInterval, 0);
-            canvas.drawArc(ovalBottom, 180, -180, false, paint);
-            posBottom.offset(vInterval, 0);
-        }
-    }
-
-    /**
-     * 상단에서 하단으로 타원조각 그리기
-     * @param canvas
-     * @param vRect 구름 형상이 그려질 기준 사각형 뷰 좌표
-     */
-    private void drawTopToBottom(Canvas canvas, final RectF vRect, final float vInterval) {
-        final float offset = vInterval / 2.0f;
-
-        PointF posLeft = new PointF(vRect.left, vRect.top);
-        PointF posRight = new PointF(vRect.right, vRect.top);
-        RectF ovalLeft = new RectF();
-        RectF ovalRight = new RectF();
-
-        final int cnt = Math.round(vRect.height() / vInterval);
-        for (int i = 0; i < cnt; ++i) {
-            if (i == cnt - 1) {
-                ovalLeft.set((posLeft.x - offset), posLeft.y, (posLeft.x + offset), vRect.bottom);
-                ovalRight.set((posRight.x - offset), posRight.y, (posRight.x + offset), vRect.bottom);
-            } else {
-                ovalLeft.set((posLeft.x - offset), posLeft.y, (posLeft.x + offset), (posLeft.y + vInterval));
-                ovalRight.set((posRight.x - offset), posRight.y, (posRight.x + offset), (posRight.y + vInterval));
-            }
-            canvas.drawArc(ovalLeft, 270, -180, false, paint);
-            posLeft.offset(0, vInterval);
-            canvas.drawArc(ovalRight, 270, 180, false, paint);
-            posRight.offset(0, vInterval);
-        }
-    }
-
-
 
     /**
      * 페인트 설정
@@ -141,5 +110,20 @@ public class DrawViewCloud extends BaseDrawView {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    /**
+     * 호 조각 정보 클래스
+     */
+    private class ArcPiece {
+        public RectF oval;
+        public float startAngle;
+        public float sweepAngle;
+
+        public ArcPiece(RectF oval, float startAngle, float sweepAngle) {
+            this.oval = oval;
+            this.startAngle = startAngle;
+            this.sweepAngle = sweepAngle;
+        }
     }
 }
