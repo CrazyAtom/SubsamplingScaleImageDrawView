135a149d25d980fe031a203e05b7a6236aae4452
HANGIL-KHB\crazy <crazyatom84@gmail.com>
2017-08-16 16:09:27 +0900

    다중선택시 drawView에 번호 부여

--- subsamplingscaleimagedrawview/src/main/java/com/github/crazyatom/subsamplingscaleimagedrawview/views/DrawViewArrayAdapter.java
+++ subsamplingscaleimagedrawview/src/main/java/com/github/crazyatom/subsamplingscaleimagedrawview/views/DrawViewArrayAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.github.crazyatom.subsamplingscaleimagedrawview.R;
 import com.github.crazyatom.subsamplingscaleimagedrawview.drawviews.BaseDrawView;
@@ -23,7 +26,8 @@ import com.github.crazyatom.subsamplingscaleimagedrawview.util.Utillity;
 public class DrawViewArrayAdapter extends ArrayAdapter<BaseDrawView> {
 
     private final int resource;
-    private ArrayList<BaseDrawView> items = new ArrayList<>();
+    private Map<BaseDrawView.DrawViewType, ArrayList<BaseDrawView>> map = new HashMap<>();
+    private ArrayList<Pair<String, BaseDrawView>> items = new ArrayList<>();
 
     public DrawViewArrayAdapter(@NonNull Context context, @LayoutRes int resource) {
         super(context, resource);
@@ -32,7 +36,16 @@ public class DrawViewArrayAdapter extends ArrayAdapter<BaseDrawView> {
 
     @Override
     public void add(@Nullable BaseDrawView object) {
-        items.add(object);
+        if (map.containsKey(object.getType()) == false) {
+            map.put(object.getType(), new ArrayList<BaseDrawView>());
+        } else {
+            map.get(object.getType()).add(object);
+        }
+
+        String name = object.getName(false) + " " + (map.get(object.getType()).size() + 1);
+        items.add(Pair.create(name, object));
+
+//        items.add(Pair.create(object.getName(false), object));
     }
 
     @NonNull
@@ -42,10 +55,14 @@ public class DrawViewArrayAdapter extends ArrayAdapter<BaseDrawView> {
         if (drawView != null) {
             LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = layoutInflater.inflate(resource, null);
-            TextView name = (TextView) view.findViewById(R.id.text1);
-            name.setText(drawView.getName(false));
-            TextView info = (TextView) view.findViewById(R.id.text2);
-            info.setText("by" + drawView.getCreater() + " " + Utillity.getTimeString(drawView.getUpdateTime(), null));
+
+            TextView tvName = (TextView) view.findViewById(R.id.text1);
+            String name = map.get(drawView.getType()).size() > 0 ? items.get(position).first : drawView.getName(false);
+            tvName.setText(name);
+//            tvName.setText(drawView.getName(false));
+
+            TextView tvInfo = (TextView) view.findViewById(R.id.text2);
+            tvInfo.setText("by" + drawView.getCreater() + " " + Utillity.getTimeString(drawView.getUpdateTime(), null));
             return view;
         }
         return convertView;
@@ -54,7 +71,7 @@ public class DrawViewArrayAdapter extends ArrayAdapter<BaseDrawView> {
     @Nullable
     @Override
     public BaseDrawView getItem(int position) {
-        return items.get(position);
+        return items.get(position).second;
     }
 
     @Override
